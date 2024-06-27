1a2a9cc765991237de044787789f752a8e457121
shy <temp2stuff@gmail.com>
2024-06-26 21:04:49 -0500

    update

--- src/main/java/la/burden/holyteams/command/HolyTeamsCommand.java
+++ src/main/java/la/burden/holyteams/command/HolyTeamsCommand.java
@@ -1,5 +1,6 @@
 package la.burden.holyteams.command;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,7 +12,33 @@ public class HolyTeamsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if (sender instanceof Player) { sender.sendMessage(ChatColor.AQUA + "HolyTeams command"); }
+        //if (sender instanceof Player) {
+//
+        //    sender.sendMessage(ChatColor.AQUA + "HolyTeams command");
+//
+        //    if(args.length == 1) {
+        //        String playerName = args[0];
+        //        Player target = Bukkit.getServer().getPlayerExact(playerName);
+//
+        //        if(target == null) { sender.sendMessage(ChatColor.GREEN + "Player isn't online"); }
+        //        else { sender.sendMessage(ChatColor.YELLOW + "You've invited " + target.getDisplayName()); }
+        //    }
+        //}
+
+        if(sender instanceof Player) {
+            switch (args.length) {
+                case 0:
+                    sender.sendMessage(ChatColor.AQUA + "HolyTeams command");
+                    break;
+                case 1:
+                    String playerName = args[0];
+                    Player target = Bukkit.getServer().getPlayerExact(playerName);
+
+                    if(target == null) { sender.sendMessage(ChatColor.GREEN + "Player isn't online"); }
+                    else { sender.sendMessage(ChatColor.YELLOW + "You've invited " + target.getDisplayName()); }
+                    break;
+            }
+        }
 
         return true;
     }
